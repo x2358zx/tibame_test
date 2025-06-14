589d10b37380bac442424949dbc53b8a27956e53
Rita_Chen <x2358zx73@gmail.com>
2025-06-14 15:10:09 +0800

    改名稱為githun name

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="test",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
-    author="rita",  # Optional
+    author="Rita_Chen",  # Optional
     author_email="x2358zx73@gmail.com",  # Optional
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
